561a123ff101e6c2b752cda05bfe75539d794e33
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-04-29 16:29:05 +0200

    Another update

--- TP.Database/DatabaseSetupExtension.cs
+++ TP.Database/DatabaseSetupExtension.cs
@@ -7,28 +7,27 @@ namespace TP.Database;
 
 public static class DatabaseSetupExtension
 {
-
     public static async Task SetupDatabaseAsync(this IHost webHost)
     {
-        using IServiceScope freshScope = webHost.Services.CreateScope();
-        AppDbContext appDbContext = freshScope.ServiceProvider.GetRequiredService<AppDbContext>();
-        ILoggerFactory loggerFactory = freshScope.ServiceProvider.GetRequiredService<ILoggerFactory>();
-        ILogger logger = loggerFactory.CreateLogger("SetupDatabaseAsync");
+        using var scope = webHost.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        var dbContext = services.GetRequiredService<AppDbContext>();
+        var logger = services.GetRequiredService<ILogger<AppDbContext>>();
 
         try
         {
-            await appDbContext.Database.EnsureCreatedAsync();
-            if ((await appDbContext.Database.GetPendingMigrationsAsync()).Any())
-            {
-                logger.LogInformation("Migrating the database");
-                await appDbContext.Database.MigrateAsync();
-            }
+            var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+            if (!pendingMigrations.Any()) return;
+
+            logger.LogInformation("Pending migrations found. Applying migrations to the database");
+            await dbContext.Database.MigrateAsync();
+            logger.LogInformation("Database migration completed successfully");
         }
         catch (Exception e)
         {
-            logger.LogError(e, "Error while migrating the database");
+            logger.LogError(e, "An error occurred while migrating the database");
             throw;
         }
     }
-
 }
